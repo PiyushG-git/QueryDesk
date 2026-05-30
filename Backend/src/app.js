f7e42a0f65ddc77b6427b8ae468de2f84c7d8eda
PiyushG-git <piyushg251004@gmail.com>
2026-05-30 23:38:08 +0530

    added frontend api in backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://query-desk-puce.vercel.app/"],
     credentials: true,
     methods: [ "GET", "POST", "PUT", "DELETE" ],
 }))
