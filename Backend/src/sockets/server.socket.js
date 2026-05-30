70278839182f5295a270ee9301a18c18f51f9538
PiyushG-git <piyushg251004@gmail.com>
2026-05-30 23:51:24 +0530

    add vercel url in server.socket.js

--- Backend/src/sockets/server.socket.js
+++ Backend/src/sockets/server.socket.js
@@ -6,7 +6,7 @@ let io;
 export function initSocket(httpServer) {
     io = new Server(httpServer, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: ["http://localhost:5173", "https://query-desk-puce.vercel.app"],
             credentials: true,
         }
     })
